192dc6e75f8a1880faa90f0a8f621b0b2d3d2063
song22ee <ann7335@naver.com>
2022-04-13 03:21:28 +0900

    기능::실제날짜 적용된 달력 js 송가현
    1. input js 는 할 예정입니다.
    2. 실제날짜 적용하는 js
    1) 실제날짜 불러와 적용시키기
    2) 현재날짜 표시하기
    3)지난달과 다음달 날짜는 흐리게 표시하기
    4) 지난달, 다음달로 넘어가기
    3. css부분은 더 수정할 예정입니다.

---
+++ app/src/public/js/home/main.js
@@ -0,0 +1,103 @@
+let date = new Date(); //현재 날짜와 시간
+
+//현재 년도와 날짜
+const renderCalendar = () => {
+  const viewYear = date.getFullYear(); //2022년
+  const viewMonth = date.getMonth(); //3월 (0부터 시작)
+
+  document.querySelector('.year-month').textContent =
+    viewYear + '년 ' + (viewMonth + 1) + '월';
+
+  //지난달의 마지막 날의 Date 객체
+  const prevLast = new Date(viewYear, viewMonth, 0); //2022년 3월 31일
+  //이번달의 마지막 날의 Date 객체
+  const thisLast = new Date(viewYear, viewMonth + 1, 0); //2022 4월 30일
+
+  const PLDate = prevLast.getDate(); //지난달 마지막 달의 날짜 -31일
+  const PLDay = prevLast.getDay(); //getDay()는 요일을 반환, 목요일 (0은 일요일)
+
+  const TLDate = thisLast.getDate(); //이번달 마지막 달의 날짜 30일
+  const TLDay = thisLast.getDay(); //토요일
+
+  const prevDates = [];
+  const thisDates = [...Array(TLDate + 1).keys()].slice(1); //배열안의 개수는 날짜 개수와 동일 30일 or 31일
+  //slice()는 제일앞에 있는 0을 없애주기 위함.
+  const nextDates = [];
+
+  //지난달을 표현할 날짜들
+  if (PLDay !== 6) {
+    //지난달의 요일이 툐요일이 아니면(지난달이 토요일이면 굳이 그릴필요 없으므로)
+    for (let i = 0; i < PLDay + 1; i++) {
+      prevDates.unshift(PLDate - i);
+    }
+  }
+  //PLDay가 0일때는(일요일) prevDates=[31]
+  //1일때는(월) [30,31]
+  //5일때는(금) [26,27,28,29,30,31]
+
+  //다음달을 표현할 날짜들
+  for (let i = 1; i < 7 - TLDay; i++) {
+    nextDates.push(i);
+  }
+  //
+
+  // Dates 합치기
+  const dates = prevDates.concat(thisDates, nextDates);
+
+  const dailyReturns__text = '일일 매출액 : ';
+  let dailyReturns__figure = '';
+  //주간 매출액을 일요일만 뜨게 해야함.
+  const weeklyReturns__text = '주간 매출액 : ';
+  let weeklyReturns__figure = '';
+
+  //지난 날짜, 다음 날짜 흐리게
+  const firstDateIndex = dates.indexOf(1); //5 (1일)
+  const lastDateIndex = dates.lastIndexOf(TLDate); //34 (30일)
+  dates.forEach((date, i) => {
+    const condition =
+      i >= firstDateIndex && i < lastDateIndex + 1 ? 'this' : 'other';
+
+    dates[i] = `<div class="date ${condition}">
+      <span class="${condition}">${date}</span>
+      <div class="daily-returns returns">
+          <span class="daily-returns__title ${condition}">${dailyReturns__text}</span>
+          <span class="daily-returns__figure ${condition}">${dailyReturns__figure}</span>
+      </div>
+      <div class="weekly-returns returns">
+          <span class="weekly-returns__title ${condition}">${weeklyReturns__text}</span>
+          <span class="weekly-returns__figure ${condition}">${weeklyReturns__figure}</span>
+      </div>
+    </div>`;
+  });
+
+  document.querySelector('.dates').innerHTML = dates.join('');
+
+  // 오늘 날짜 그리기
+  const today = new Date();
+  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+    for (let date of document.querySelectorAll('.this')) {
+      if (+date.innerText === today.getDate()) {
+        date.classList.add('today');
+        break;
+      }
+    }
+  }
+};
+
+renderCalendar();
+
+//지난달, 다음날, 오늘로 이동하는 함수
+const prevMonth = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+};
+
+const nextMonth = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+};
+
+const goToday = () => {
+  date = new Date();
+  renderCalendar();
+};
